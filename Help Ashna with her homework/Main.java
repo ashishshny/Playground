ed59ebfc8400d6ddcb54210b8a2489393bd6f910
ashishshny <64847544+ashishshny@users.noreply.github.com>
2020-05-17 18:08:27 +0530

    Test code pushed to repository.

---
+++ Help Ashna with her homework/Main.java
@@ -0,0 +1,17 @@
+#include<iostream>
+int main()
+{
+  int a,b,c;
+  std::cin>>a>>b>>c;
+  c=a+b;
+  std::cout<<"a+b="<<(a+b);
+  c=a-b;
+  std::cout<<"\na-b="<<(a-b);
+  c=a*b;
+  std::cout<<"\na*b="<<(a*b);
+  c=a/b;
+  std::cout<<"\na/b="<<(a/b);
+  c=a%b;
+  std::cout<<"\na%b="<<(a%b);
+}
+  
